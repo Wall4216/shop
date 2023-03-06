7cd56016a2757e64c60f319ac24c52ae680b05a8
Wall <ilnazamirhanov61@gmail.com>
2023-03-06 23:04:47 +0300

    add views for color

---
+++ shop/resources/views/color/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.main')
+
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Редактировать цвет</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Главная</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <form action="{{route('color.update', $color->id)}}" method="post">
+                    @csrf
+                    @method('patch')
+                    <div class="form-group">
+                        <input type="text" class="form-control" value="{{$color->title}}" name="title" placeholder="Наименование">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="form-control" value="Редактировать">
+                    </div>
+                </form>
+                <!-- ./col -->
+            </div>
+            <!-- /.row -->
+            <!-- Main row -->
+            <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+    </section>
+@endsection

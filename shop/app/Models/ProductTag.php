abed67577926ef24a631ee15a584b2af06ae5365
Wall <ilnazamirhanov61@gmail.com>
2023-03-07 23:47:13 +0300

    add models

--- shop/app/Models/ProductTag.php
+++ shop/app/Models/ProductTag.php
@@ -5,10 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model
+class ProductTag extends Model
 {
-    use HasFactory;
-    protected $table = 'tags';
-
+    protected $table = 'product_tags';
     protected $guarded = false;
 }

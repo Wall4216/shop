81bb175fe00919a8dc19fb654c6b7d4e7e986af9
Wall <ilnazamirhanov61@gmail.com>
2023-03-10 00:35:20 +0300

    show product

--- shop_vvue/src/views/product/Index.vue
+++ shop_vvue/src/views/product/Index.vue
@@ -239,14 +239,14 @@
                                       class="flaticon-left-and-right-arrows"></i>
                                     <span>
                                                                             compare</span> </a> </li>
-                                  <li><a href="#popup5" class="popup_link"> <i
+                                  <li><a :href="`#popup${product.id}`" class="popup_link"> <i
                                       class="flaticon-visibility"></i>
                                     <span> quick view</span>
                                   </a> </li>
                                 </ul>
                               </div>
                             </div>
-                            <div id="popup5" class="product-gird__quick-view-popup mfp-hide">
+                            <div :id="`popup${product.id}`" class="product-gird__quick-view-popup mfp-hide">
                               <div class="container">
                                 <div class="row justify-content-between align-items-center">
                                   <div class="col-lg-6">
@@ -257,17 +257,17 @@
                                             <li
                                                 class="tab-nav popup-product-thumb">
                                               <a href="#tabb1">
-                                                <img src="src/assets/images/shop/products-v6-img5.jpg"
+                                                <img :src="product.image_url"
                                                      alt="" /> </a> </li>
                                             <li
                                                 class="tab-nav popup-product-thumb ">
                                               <a href="#tabb2">
-                                                <img src="src/assets/images/shop/products-v6-img6.jpg"
+                                                <img :src="product.image_url"
                                                      alt="" /> </a> </li>
                                             <li
                                                 class="tab-nav popup-product-thumb ">
                                               <a href="#tabb3">
-                                                <img src="src/assets/images/shop/products-v6-img7.jpg"
+                                                <img :src="product.image_url"
                                                      alt="" /> </a> </li>
                                           </ul>
                                         </div>
@@ -276,21 +276,21 @@
                                                class="tab-item popup-product-image">
                                             <div
                                                 class="popup-product-single-image">
-                                              <img src="src/assets/images/shop/products-v6-img5.jpg"
+                                              <img :src="product.image_url"
                                                    alt="" /> </div>
                                           </div>
                                           <div id="tabb2"
                                                class="tab-item popup-product-image">
                                             <div
                                                 class="popup-product-single-image">
-                                              <img src="src/assets/images/shop/products-v6-img6.jpg"
+                                              <img :src="product.image_url"
                                                    alt="" /> </div>
                                           </div>
                                           <div id="tabb3"
                                                class="tab-item popup-product-image">
                                             <div
                                                 class="popup-product-single-image">
-                                              <img src="src/assets/images/shop/products-v6-img7.jpg"
+                                              <img :src="product.image_url"
                                                    alt="" /> </div>
                                           </div> <button class="prev"> <i
                                             class="flaticon-back"></i>
@@ -303,7 +303,7 @@
                                   </div>
                                   <div class="col-lg-6">
                                     <div class="popup-right-content">
-                                      <h3>Brown Office Shoe</h3>
+                                      <h3>{{ product.title }}</h3>
                                       <div class="ratting"> <i
                                           class="flaticon-star"></i> <i
                                           class="flaticon-star"></i> <i
@@ -311,11 +311,10 @@
                                         <i class="flaticon-star"></i> <i
                                             class="flaticon-star"></i>
                                         <span>(112)</span> </div>
-                                      <p class="text"> Hydrating Plumping Intense
-                                        Shine Lip Colour
+                                      <p class="text"> {{product.description}}
                                       </p>
                                       <div class="price">
-                                        <h2> $42 USD <del> $65 USD</del></h2>
+                                        <h2> ${{ product.price}} <del> ${{product.old_price}}</del></h2>
                                         <h6> In stuck</h6>
                                       </div>
                                       <div class="color-varient"> <a href="#0"
@@ -2139,7 +2138,11 @@ export default {
           .then(res => {
             this.products = res.data.data
       })
+      .finally(v => {
+            $(document).trigger('change')
+      })
     }
+
   }
 
 }

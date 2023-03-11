f731b9b484b203d4a50c4fbe00612927529d3f42
Wall <ilnazamirhanov61@gmail.com>
2023-03-11 23:58:31 +0300

    add backend for groups

--- shop/app/Http/Controllers/API/Product/IndexController.php
+++ shop/app/Http/Controllers/API/Product/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Product\IndexProductResource;
 use App\Http\Resources\Product\ProductResource;
 use App\Models\Product;
 use App\Models\Tag;
@@ -13,6 +14,6 @@ class IndexController extends Controller
     public function __invoke()
     {
         $products = Product::all();
-        return ProductResource::collection($products);
+        return IndexProductResource::collection($products);
     }
 }

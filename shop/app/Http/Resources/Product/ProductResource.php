0d7147c7505d91c0b0ecdd0998e75c15795107dc
Wall <ilnazamirhanov61@gmail.com>
2023-03-12 21:24:09 +0300

    add image get

--- shop/app/Http/Resources/Product/ProductResource.php
+++ shop/app/Http/Resources/Product/ProductResource.php
@@ -28,6 +28,7 @@ class ProductResource extends JsonResource
             'count' => $this->count,
             'is_published' => $this->is_published,
             'category_title' => new CategoryResource($this->category),
+            'product_images' => ProductImageResource::collection($this->productImages),
             'group_products' => ProductMinResource::collection($products),
         ];
     }
